b3fc1201155cb53aee5e7d018a0748b9384d626e
Lam Nguyen <lamnh@pixiostudio.com>
2019-05-29 23:54:40 +0700

    Finished community page

---
+++ src/main/webapp/js/community.js
@@ -0,0 +1,22 @@
+function buildList(userList) {
+    const text = userList.map((user) => {
+        return `
+            <li>
+                <a href="/user-page.html?user=${user}">${user}</a>
+            </li>
+        `;
+    }).join("");
+    document.getElementsByClassName("user-list")[0].innerHTML = text;
+}
+
+function fetchUserList() {
+    fetch("/user-list")
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw res.statusText;
+        })
+        .then(buildList)
+        .catch(alert)
+}
