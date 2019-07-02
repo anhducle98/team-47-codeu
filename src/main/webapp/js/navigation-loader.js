9f629dd4e3d215cb1e0cda05ae670ed23291d563
anhducle <anhducle@coccoc.com>
2019-07-02 19:01:22 +0700

    exclude images and hashtags from translation

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -2,6 +2,8 @@ function requestTranslation(div) {
   const post = div.parentElement.parentElement;
   const text = encodeURIComponent(
     post.getElementsByClassName("post-content--text")[0].innerHTML
+      .replace(/<img .*?>/g, "\n")
+      .replace(/<p>Suggested tags: .*?<\/p>/g, "\n")
   );
   const queryURL = `/translate?text=${text}&sourceLanguageCode=en&targetLanguageCode=vi`;
   fetch(queryURL, { method: "POST" })
